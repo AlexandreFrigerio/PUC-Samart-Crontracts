8d25996405aad7e2a468e624cb292bb806dc2abe
Alexandre Frigerio <92129747+AlexandreFrigerio@users.noreply.github.com>
2021-10-14 18:06:43 -0300

    Create CarteiraOAB.sol

---
+++ CarteiraOAB.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.8.9;
+
+contract CarteiraOAB {
+    //autor: Alexandre Frigerio
+    string constant public nome = "Luiz Gonzaga Pinto Gama";
+    uint public inscricao = 123456;
+    string public filiacao_mae = "Luiza Mahin";
+    string public filiacao_pai = "Benedito Graco Pinto da Gama";
+    string public naturalidade = "Salvador - BA";
+    string public nascimento = "21/06/1980";
+    uint private rg_sem_digito = 43170456;
+    uint private cpf_apenas_numeros = 30830130530;
+    string public expedido_em;
+    
+    
+}
