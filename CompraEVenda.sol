90b94815cd07864bbb89e727367cf08e214c029c
Alexandre Frigerio <92129747+AlexandreFrigerio@users.noreply.github.com>
2021-10-19 22:07:36 -0300

    Update CompraEVenda.sol

--- CompraEVenda.sol
+++ CompraEVenda.sol
@@ -4,23 +4,58 @@ contract CompraEVenda {
     // Autor: Alexandre
     string comprador;
     string vendedor;
-    string matricula;
-    string cartorio;
+    string public matricula;
+    string public cartorio;
     string dataDeVencimento;
+    
     bool quitado = false;
-    uint public valorTotal = 100000;
+    
+    uint public valorTotal;
     uint public valorDaEntrada;
-    uint public valorEmAberto;
     uint public quntidadeDeParcelas;
+    uint public porcentagemDaMulta;
     uint public valorDaParcela;
-    uint porcentagemDaMulta;
+    uint public valorEmAberto;
+    
+    constructor(
+        uint _valorTotal,
+        uint _valorDaEntrada,
+        uint _quantidadeDeParcelas,
+        string memory _matricula,
+        string memory _cartorio
+   
+        )
+    {
+        valorTotal = _valorTotal;
+        valorDaEntrada = _valorDaEntrada;
+        quntidadeDeParcelas = _quantidadeDeParcelas;
+        matricula = _matricula;
+        cartorio = _cartorio;
+        valorEmAberto = _valorTotal;
+        valorDaParcela = funcaovalorParcela();
+        
+        
+    }
+    
     
     function pagarEntrada(uint _valorPagamento) public returns(uint, string memory){
-        valorDaEntrada = _valorPagamento;
         valorEmAberto = valorTotal - _valorPagamento;
         return(valorEmAberto, "valor em aberto");
     }
     
+    function pagarParcela(uint _valorDaParcela) public returns(uint, string memory){
+        valorEmAberto = valorEmAberto - _valorDaParcela;
+        return(valorEmAberto, "valor em aberto");
+        }
+    
+    function funcaovalorParcela() public view returns (uint){
+        uint calculoValorParcela = (valorTotal - valorDaEntrada)/quntidadeDeParcelas;
+        return(calculoValorParcela);
+    }
+    
+    function valorDaMulta(uint _porcentagemDaMulta) public view returns (uint, string memory){
+        uint multa = (_porcentagemDaMulta*valorTotal)/100;
+        return(multa, "valor da multa");
+    }
     
 }
-
